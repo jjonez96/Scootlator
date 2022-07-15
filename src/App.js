63a8def70556a62279245a1a0768ac0addeb8dd9
Joona Luukkonen <joona114@hotmail.fi>
2022-07-15 22:28:25 +0300

    New responsive UI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
 import React from "react";
-import Search from "./components/Search";
 import Map from "./components/Map";
 
 const App = () => {
   return (
     <div>
-      <Search />
       <Map />
     </div>
   );
