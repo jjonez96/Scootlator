a1f212d7053deb3099f74027cc41be2f99cce0d0
Joona Luukkonen <joona114@hotmail.fi>
2022-11-30 22:06:09 +0200

    price validation update

--- src/components/CalculationResults.jsx
+++ src/components/CalculationResults.jsx
@@ -13,10 +13,10 @@ const CalculationResults = (props) => {
 
   return (
     <>
-      {toFixedPrice === "NaN" ? (
-        <></>
+      {price === "" ? (
+        <div></div>
       ) : (
-        <div className="resultsBg bg-dark text-info container fixed-bottom d-flex justify-content-around shadow hstack gap-5">
+        <div className="resultsBg bg-dark text-light container fixed-bottom d-flex justify-content-around shadow ">
           <div className="m-1">
             Pituus <RiPinDistanceFill color="#1ef778" />
             <br />
@@ -26,23 +26,26 @@ const CalculationResults = (props) => {
             Kesto <RiTimeFill color="#1ef778" />
             <br />~{props.duration}
           </div>
-          {toFixedPrice === "NaN €" || toFixedPrice === "1 €" ? (
-            <p className="mt-3">Palvelua ei valittu</p>
+          {price === "NaN €" || price === "1 €" ? (
+            <p className="text-danger mt-3">Palvelua ei valittu</p>
           ) : (
-            <div className="m-1">
-              Hinta <RiPriceTag3Fill color="#1ef778" />
-              <br />~{toFixedPrice} €
+            <div className="m-1 d-flex justify-content-around  ">
+              <div>
+                Hinta <RiPriceTag3Fill color="#1ef778" />
+                <br />~{toFixedPrice} €
+              </div>
+              <div className="vr m-2 text-info" />
+              <div>
+                Hidasajo <GiSnail color="#1ef778" />
+                <Form.Check
+                  type="switch"
+                  onChange={setSlowMode}
+                  checked={slow}
+                  className="mx-4"
+                />
+              </div>
             </div>
           )}
-          <div className="m-1">
-            Hidasajo <GiSnail color="#1ef778" />
-            <Form.Check
-              type="switch"
-              onChange={setSlowMode}
-              checked={slow}
-              className="mx-4"
-            />
-          </div>
         </div>
       )}
     </>
